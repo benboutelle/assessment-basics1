3db06b23b9cc0c80912e4e403e4aecc71e102245
Benjamin Boutelle <bennydeltoro49@gmail.com>
2022-10-21 12:45:57 -0600

    ASSesment-js-basics-1

---
+++ gitInfo.js
@@ -0,0 +1,78 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/ 
+
+//CODE HERE
+gitDefinition = "Git is a vesion control system allows a person and teams to see what changes have been done to the code for projects, kinda like a snapshot of the history of the code "
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+gitHubDefiniton = "GitHub is a website. It allows you to take your git repositories and upload/download them. It is essentially a cloud service for your git repositories."
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+ gitInitDefinition = "initializes a local repo tells code to get in the folder"
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+gitStatusDefinition = "tells you about the repo like the history so far"
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+gitAddDefinition = "adds repo to file to be tracked "
+gitAddCode = "git add ."
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+gitCommitDefinition = "gives repo a tag to be tracked by"
+gitCommitCode = "git commit -m (comment)"
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+gitPushDefinition = "pushes the git repo to gitHub"
---
+++ sdlc.js
@@ -0,0 +1,30 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+//1 Planning
+//2 Analysis of Requirements
+//3 Design
+//4 Implementation
+//5 Testing & Integration
+//6 Maintain
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const Analysis of Requirements; = "  all the requirements for the target software are specified. These requirements get approval from the customers, market analysts, and stakeholders. "
+const Design = " Software designers come up with the best architecture for the software "
+const Implementation = "At this stage, the fundamental development of the product starts. For this, developers use a specific programming code js, python, etc"
+const Testing&Integration = "test the software to make sure it works properly tracking and fixing flaws "
+const Maintain = "testing in a real industrial enviroment to make sure it works smoothly contuning to work out flaws"
